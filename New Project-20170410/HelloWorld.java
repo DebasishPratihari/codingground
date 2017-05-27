3edad2a7cb8e418ada45710478e24a59f2363c5b
DebasishPratihari <debasishpratihari@gmail.com>
2017-05-27 21:14:37 +0530

    kshiti

--- New Project-20170410/HelloWorld.java
+++ New Project-20170410/HelloWorld.java
@@ -4,13 +4,6 @@ public class HelloWorld{
         System.out.println("Hello World");
 	   system.out.println("Added from SVN");
 system.out.println("Added by debasish");
-<<<<<<< .mine
-system.out.println("Added by debasish.....Manash");
-
 system.out.println("Reviewed by Kshiti.");
-
-system.out.println("Added by debasish");
-system.out.println("Manash..........Kuna");
-
      }
 }
